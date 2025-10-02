54b037ddc60510a6233fc3864f84449be96da5eb
Ayodeji <interpercelservice@gmail.com>
2025-10-02 23:57:25 +0100

    Terraform key-points

---
+++ Exercise2/Keypair.tf
@@ -0,0 +1,4 @@
+resource "aws_key_pair" "dev-key" {
+  key_name   = "dev-key"
+  public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFPpAF5hn+tG2LIH9mizKx+UaRNQH6C3mwls09S9mXEf Ayodeji Adebayo@CrypTechNg"
+}
